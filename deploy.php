7a45e7afc1eb678b5d5bb36f449c83f109a0a3db
Sage Imel <sage@sagenite.net>
2013-10-04 02:23:57 -0700

    Make deployment hook only fire on master

--- deploy.php
+++ deploy.php
@@ -2,9 +2,12 @@
 
 require_once(dirname(__FILE__)."/../secret.php");
 if ($secret == $_GET['secret']){
-  echo "Woot";
-  echo exec(dirname(__FILE__)."/../deploy.sh");
-  echo "Updated";
+  $json = json_decode($_POST['payload']);
+  if(preg_match('/\/master$/', $stuff->ref){
+    echo "Woot";
+    echo exec(dirname(__FILE__)."/../deploy.sh");
+    echo "Updated";
+  }
 }
 
 ?>
