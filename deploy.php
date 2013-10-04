8e8d4ee10f1aee0bb3f2b8dd0c3cb48b42382a69
Sage Imel <sage@sagenite.net>
2013-10-04 02:34:06 -0700

    Revert "Make deployment hook only fire on master", too tired to debug
    This reverts commit 7a45e7afc1eb678b5d5bb36f449c83f109a0a3db.

--- deploy.php
+++ deploy.php
@@ -2,12 +2,9 @@
 
 require_once(dirname(__FILE__)."/../secret.php");
 if ($secret == $_GET['secret']){
-  $json = json_decode($_POST['payload']);
-  if(preg_match('/\/master$/', $stuff->ref){
-    echo "Woot";
-    echo exec(dirname(__FILE__)."/../deploy.sh");
-    echo "Updated";
-  }
+  echo "Woot";
+  echo exec(dirname(__FILE__)."/../deploy.sh");
+  echo "Updated";
 }
 
 ?>
