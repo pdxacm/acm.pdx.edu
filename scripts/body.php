9107eb547edd63a33c408e777cae78510d84a39d
Sage Imel <nightfly19@gmail.com>
2013-10-19 02:17:32 -0700

    Change dev root to being whatever part of the URI is acm.pdx.edu

--- scripts/body.php
+++ scripts/body.php
@@ -1,84 +1,83 @@
 <?php
-//this is needed for links to work because of how the address systems change
-$root = "/";
-// tell me everything about the following variable: $_SERVER
-//echo var_dump($_SERVER) . "\n";
 
-// check if acm.pdx.edu or web.cecs.pdx.edu/~psuacm/
-if( strcmp($_SERVER["HTTP_HOST"], "acm.pdx.edu") != 0 ) {
-  // if web.cecs.pdx.edu/~psuacm/, then get /~psuacm/
-  $root = substr($_SERVER["PHP_SELF"], 1);
-  $root = substr($root, 0, strcspn($root, '/'));
-  $root = "/" . $root . "/";
-} else {
-  // if acm.pdx.edu
-  // $root already is "/"
+// If running on acm.pdx.edu root is root
+if( $_SERVER["HTTP_HOST"] == "acm.pdx.edu") {
+  $root = "/";
 }
-
+// If accessing at ~psuacm set that as root.
+elseif(preg_match('/^\/~psuacm\//',$_SERVER["REQUEST_URI"])){
+  $root = '/~psuacm/';
+}
+// Otherwise assume the user is running with the root as whatever part of the URI matches acm.cat.pdx.edu
+else{
+  $path_parts = array('');
+  preg_match( "/^\/acm\.cat\.pdx\.edu\/[^\/]+/", $_SERVER["REQUEST_URI"], $path_parts);
+  $root = $path_parts[0];
+}
 include 'events_list.php';
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-  <title><?php
+<title><?php
 if(isset($title))
-{
-  echo $title;
-}
+  {
+    echo $title;
+  }
 else
-{
-  echo "ACM @ PSU";
-}
+  {
+    echo "ACM @ PSU";
+  }
 ?></title>
-  <meta charset="utf-8">
-  <link type="text/css" rel="stylesheet" href="<?php echo $root; ?>global.css" />
-  <link rel="shortcut icon" href="<?php echo $root; ?>images/favicon.ico" type="image/x-icon" />
-<?php
-if(function_exists('head_content'))
-{
-  head_content();
-}
+<meta charset="utf-8">
+  <link type="text/css" rel="stylesheet" href="<?= $root ?>global.css" />
+  <link rel="shortcut icon" href="<?= $root ?>images/favicon.ico" type="image/x-icon" />
+  <?php
+  if(function_exists('head_content'))
+    {
+      head_content();
+    }
 ?>
 </head>
 <body>
-  <div id="wrapper">
-    <div id="header">
-      <div id="psu">
-        <a href="http://www.pdx.edu"><img src="<?php echo $root; ?>images/psu-logo-trans.png" alt="Portland State University (PSU) Logo" /></a>
-      </div>
-      <div id="acm">
-        <a href="http://www.acm.org"><img src="<?php echo $root; ?>images/acm-logo.jpg" alt="Association for Computing Machinery (ACM) Logo" /></a>
-      </div>
-      <h1>ACM @ PSU</h1>
-      Portland State University Student Chapter of<br />
-      the Association for Computing Machinery
-    </div>
+<div id="wrapper">
+  <div id="header">
+  <div id="psu">
+  <a href="http://www.pdx.edu"><img src="<?php echo $root; ?>images/psu-logo-trans.png" alt="Portland State University (PSU) Logo" /></a>
+  </div>
+  <div id="acm">
+  <a href="http://www.acm.org"><img src="<?php echo $root; ?>images/acm-logo.jpg" alt="Association for Computing Machinery (ACM) Logo" /></a>
+  </div>
+  <h1>ACM @ PSU</h1>
+  Portland State University Student Chapter of<br />
+  the Association for Computing Machinery
+  </div>
 
-    <div id="link_bar">
-      <table><tr>
-        <td>
-          <a href="<?php echo $root; ?>index.php">Home</a>
-        </td><td>
-          <a href="<?php echo $root; ?>about.php">About Us</a>
-        </td><td>
-          <a href="<?php echo $root; ?>contact.php">Contact Us</a>
-        </td><td>
-          <a href="<?php echo $root; ?>events.php">Events</a>
-        </td><td>
-          <a href="<?php echo $root; ?>tutors.php">Tutors</a>
-        </td><td>
-          <a href="<?php echo $root; ?>misc.php">Misc.</a>
-        </td>
-      </tr></table>
-    </div>
+  <div id="link_bar">
+  <table><tr>
+  <td>
+  <a href="<?php echo $root; ?>index.php">Home</a>
+  </td><td>
+  <a href="<?php echo $root; ?>about.php">About Us</a>
+  </td><td>
+  <a href="<?php echo $root; ?>contact.php">Contact Us</a>
+  </td><td>
+  <a href="<?php echo $root; ?>events.php">Events</a>
+  </td><td>
+  <a href="<?php echo $root; ?>tutors.php">Tutors</a>
+  </td><td>
+  <a href="<?php echo $root; ?>misc.php">Misc.</a>
+  </td>
+  </tr></table>
+  </div>
 
-    <div class="backdrop">
-      <div id="main"><div class="padding">
-<?php
-if(function_exists('main_content'))
-{
-  main_content();
-}
+  <div class="backdrop">
+  <div id="main"><div class="padding">
+  <?php
+  if(function_exists('main_content'))
+    {
+      main_content();
+    }
 
 // Manage events page
 // I've tried to get this to work through just the events.php file.
@@ -86,117 +85,117 @@ if(function_exists('main_content'))
 // If you can figure out how to do this in the events page instead and
 // keep the sidebar events list working, please do.
 if(strpos($_SERVER['REQUEST_URI'], $root."events.php") !== false)
-{
-  if(isset($events_array))
   {
-    // if a specific event is requested
-    if(isset($_REQUEST['event']))
-    {
-      $s = sizeof($events_array);
-      if((int) $_REQUEST['event'] > 0 && 
-         (int) $_REQUEST['event'] <= $s)
+    if(isset($events_array))
       {
-        $e = $events_array[ $s - (int) $_REQUEST['event'] ];
-        echo "        <p><b>" . $e->title . "</b><br />\n";
-        echo "        &#160; <sub>" . $e->date->format('m/d/Y');
-        echo " - by " . $e->speaker . "</sub><br />\n";
-        echo "<p>" . $e->description . "</p>\n";
-      }
-    }
-    else // general page display
-    {
-      $events_string = "";
-      $s = sizeof($events_array);
+        // if a specific event is requested
+        if(isset($_REQUEST['event']))
+          {
+            $s = sizeof($events_array);
+            if((int) $_REQUEST['event'] > 0 &&
+               (int) $_REQUEST['event'] <= $s)
+              {
+                $e = $events_array[ $s - (int) $_REQUEST['event'] ];
+                echo "        <p><b>" . $e->title . "</b><br />\n";
+                echo "        &#160; <sub>" . $e->date->format('m/d/Y');
+                echo " - by " . $e->speaker . "</sub><br />\n";
+                echo "<p>" . $e->description . "</p>\n";
+              }
+          }
+        else // general page display
+          {
+            $events_string = "";
+            $s = sizeof($events_array);
 
-      // future events
-      foreach ($events_array as &$e)
+            // future events
+            foreach ($events_array as &$e)
+              {
+                if($e->date->getTimestamp() >= $cdate->getTimestamp())
+                  {
+                    $events_string = "        <p><b><a href=\"" . $root .
+                      "events.php?event=" . $s . "\">" . $e->title . "</a></b><br />\n" .
+                      "        &#160; <sub>" . $e->date->format('m/d/Y') .
+                      " - by " . $e->speaker . "</sub></p>\n\n" . $events_string;
+                  }
+                else
+                  break;
+
+                $s--;
+              }
+            unset($e);
+
+            if($events_string != "")
+              $events_string = "        <h3>Upcoming Events</h3>\n\n" . $events_string;
+            if($s > 0)
+              $events_string =  $events_string ."        <hr />\n\n        <h3>Past Events</h3>\n\n";
+
+            $s = sizeof($events_array);
+
+            // past events
+            foreach ($events_array as &$e)
+              {
+                if($e->date->getTimestamp() < $cdate->getTimestamp())
+                  {
+                    $events_string =  $events_string . "        <p><b><a href=\"" . $root .
+                      "events.php?event=" . $s . "\">" . $e->title . "</a></b><br />\n" .
+                      "        &#160; <sub>" . $e->date->format('m/d/Y') .
+                      " - by " . $e->speaker . "</sub></p>\n\n";
+                  }
+
+                $s--;
+              }
+            unset($e);
+
+            echo $events_string;
+          }
+      }
+    else
       {
-        if($e->date->getTimestamp() >= $cdate->getTimestamp())
-        {
-          $events_string = "        <p><b><a href=\"" . $root .
-            "events.php?event=" . $s . "\">" . $e->title . "</a></b><br />\n" .
-            "        &#160; <sub>" . $e->date->format('m/d/Y') . 
-            " - by " . $e->speaker . "</sub></p>\n\n" . $events_string;
-        }
-        else
-          break;
-
-        $s--;
+        echo "        <p>Event List currently offline.</p>";
       }
-      unset($e);
-
-      if($events_string != "")
-        $events_string = "        <h3>Upcoming Events</h3>\n\n" . $events_string;
-      if($s > 0)
-        $events_string =  $events_string ."        <hr />\n\n        <h3>Past Events</h3>\n\n";
-
-      $s = sizeof($events_array);
-
-      // past events
-      foreach ($events_array as &$e)
-      {
-        if($e->date->getTimestamp() < $cdate->getTimestamp())
-        {
-          $events_string =  $events_string . "        <p><b><a href=\"" . $root .
-            "events.php?event=" . $s . "\">" . $e->title . "</a></b><br />\n" .
-            "        &#160; <sub>" . $e->date->format('m/d/Y') .
-            " - by " . $e->speaker . "</sub></p>\n\n";
-        }
-
-        $s--;
-      }
-      unset($e);
-
-      echo $events_string;
-    }
   }
-  else
-  {
-    echo "        <p>Event List currently offline.</p>";
-  }
-}
 
 ?>
-      </div></div>
+</div></div>
 
-      <div id="sidebar"><div class="padding">
-        <h3>Upcoming Events:</h3><hr />
+<div id="sidebar"><div class="padding">
+  <h3>Upcoming Events:</h3><hr />
 
-<?php
-if(isset($events_array))
-{
-  $side_string = "";
-  $s = sizeof($events_array);
-  
-  foreach ($events_array as &$e)
-  {
-    if($e->date->getTimestamp() >= $cdate->getTimestamp())
+  <?php
+  if(isset($events_array))
     {
-      $side_string = "        <p>" . $e->date->format('D, M d, Y') . "<br />\n" .
-                     "        <a href=\"" . $root . "events.php?event=" . $s .
-                     "\">" . $e->title . "</a></p>\n\n" . $side_string;
-    }
-    else
-      break;
+      $side_string = "";
+      $s = sizeof($events_array);
 
-    $s--;
-  }
-  unset($e);
-  echo $side_string;
-}
-else
-  echo "No new events currently schedules<br />\n";
+      foreach ($events_array as &$e)
+        {
+          if($e->date->getTimestamp() >= $cdate->getTimestamp())
+            {
+              $side_string = "        <p>" . $e->date->format('D, M d, Y') . "<br />\n" .
+                "        <a href=\"" . $root . "events.php?event=" . $s .
+                "\">" . $e->title . "</a></p>\n\n" . $side_string;
+            }
+          else
+            break;
+
+          $s--;
+        }
+      unset($e);
+      echo $side_string;
+    }
+  else
+    echo "No new events currently schedules<br />\n";
 
 /*        <iframe src="https://www.google.com/calendar/embed?height=120&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=psuacm%40cs.pdx.edu&amp;color=%23B1440E&amp;ctz=America%2FLos_Angeles"
           style=" border-width:0 " width="180px" height="120px" frameborder="0" scrolling="no"></iframe>
 
- I can't make this google calendar fit in the sidebar in any usefull way.*/
+          I can't make this google calendar fit in the sidebar in any usefull way.*/
 ?>
-        <hr />
-        
-        Looking for a Job or internship? Check out 
-        <a href="http://www.pdx.edu/computer-science/job-resources">MCECS's Job Resources Page</a>.
-        
+<hr />
+
+Looking for a Job or internship? Check out
+<a href="http://www.pdx.edu/computer-science/job-resources">MCECS's Job Resources Page</a>.
+
         <hr />
 
         <p>Sign up for the
