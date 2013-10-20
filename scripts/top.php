1f590fe86b6049d39b37a31a20bb4d2cc87519c4
Sage Imel <nightfly19@gmail.com>
2013-10-20 14:36:27 -0700

    Remove debug statement

--- scripts/top.php
+++ scripts/top.php
@@ -1,7 +1,6 @@
 <?php require_once(dirname(__FILE__).'/acm.php') ?>
 <?php require_once(dirname(__FILE__).'/events.php') ?>
 <!DOCTYPE html>
-<?php echo $root ?>
 <html>
   <head>
     <title><?=$title?></title>
