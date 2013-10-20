236ad0576f679cc1b661ffd40a4d70b619204baa
Sage Imel <nightfly19@gmail.com>
2013-10-20 14:40:36 -0700

    Add a $production global variable

--- scripts/acm.php
+++ scripts/acm.php
@@ -3,16 +3,19 @@
 // If running on acm.pdx.edu root is root
 if( $_SERVER["HTTP_HOST"] == "acm.pdx.edu") {
   $root = "";
+  $production = true;
 }
 // If accessing at ~psuacm set that as root.
 elseif(preg_match('/^\/~psuacm\//',$_SERVER["REQUEST_URI"])){
   $root = '/~psuacm';
+  $production = true;
 }
 // Otherwise assume the user is running with the root as whatever part of the URI matches acm.cat.pdx.edu
 else{
   $path_parts = array('');
   preg_match( "/^(.*)acm\.pdx\.edu/", $_SERVER["REQUEST_URI"], $path_parts);
   $root = $path_parts[0].'';
+  $production = false;
 }
 
 $file_root=dirname(dirname(__FILE__));
