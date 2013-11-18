d2e89d140cf9711d7da002a4b397c1cc3bbea9fa
Azadi Sean Walsh <seanwalsh1984@gmail.com>
2013-11-17 22:59:18 -0800

    Removed XNA from Misc. Section

--- misc.php
+++ misc.php
@@ -16,6 +16,4 @@ require_once(dirname(__FILE__).'/scripts/top.php')
         <p>Here is the link for the
         <a href="https://sites.google.com/a/pdx.edu/siampdx/">Society for Industrial and Applied Mathematics</a>
         here at PSU.</p>
-        We meet up and do XNA projects on Fridays at 4:30PM when there are no ACM speakers,
-        and on Saturdays at 2:30PM when there are speakers. Meetups occur in the Windows Lab in FAB 88-02.
 <?php require_once(dirname(__FILE__).'/scripts/bottom.php') ?>
