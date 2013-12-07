7946e6acfa86a8551ae7ebb8add7a8e95f9e3911
Sage Imel <nightfly19@gmail.com>
2013-12-06 17:05:37 -0800

    Add sourcecode link to events pages

--- event.php
+++ event.php
@@ -8,6 +8,7 @@
     <div class='event_full'>
       <h1><?=$event['title']?></h1>
       <sub><?=$event['date']->format('m/d/Y')?> - <?=$event['speaker']?></sub>
+      <?php if($event['github']){?><p><a href="<?=$event['github']?>">Source code</a></p><?php }?>
       <?=\Michelf\Markdown::defaultTransform($event['description'])?>
     </div>
 <?php }
