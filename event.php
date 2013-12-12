bc84d0df131cc8c6dc34f54071641235ab7bae42
Azadi Sean Walsh <seanwalsh1984@gmail.com>
2013-12-12 11:02:24 -0800

    Modified YAML setttings

--- event.php
+++ event.php
@@ -9,7 +9,10 @@
       <h1><?=$event['title']?></h1>
       <sub><?=$event['date']->format('m/d/Y')?> - <?=$event['speaker']?></sub>
       <?php if($event['github']){?><p><a href="<?=$event['github']?>">Source code</a></p><?php }?>
-      <?=\Michelf\Markdown::defaultTransform($event['description'])?>
+	  <?php if($event['tutorial']){?><p><a href="<?=$event['tutorial']?>">Tutorial</a></p><?php }?>
+	  <?php if($event['slides']){?><p><a href="<?=$event['slides']?>">Slides</a></p><?php }?>
+      <?php if($event['ndas']){?><p><a href="<?=$event['ndas']?>"></a></p><?php }?>
+<?=\Michelf\Markdown::defaultTransform($event['description'])?>
     </div>
 <?php }
 require_once(dirname(__FILE__).'/scripts/bottom.php')
