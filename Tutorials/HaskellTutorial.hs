0bf8c501c8f558889babb72bf4398bb52b3ed161
Azadi Sean Walsh <seanwalsh1984@gmail.com>
2013-12-12 10:57:57 -0800

    Added more material to Haskell tutorial

--- Tutorials/HaskellTutorial.hs
+++ Tutorials/HaskellTutorial.hs
@@ -62,4 +62,38 @@ let b = [2, 4, 6, 8, 10]
 -- a ++ b OR b ++ a
 -- The first will put all of a in the front of b, and the other will do -- just the opposite.
 
+-- ExplicitTypes (declaration and initialization)
+3 :: Double
+-- Yields a decimal type containing a value of 3.0
+-- Whereas
+3 :: Int
+-- Yields an integer type containing a value of 3
+-- Other examples
+True :: Bool
+c :: Char
+s :: String
+
+-- Recalling the last interpreted result
+-- You can recall the last result using 'it'
+-- Exercise: Using it
+-- Enter the following lines of code into Haskell
+5
+it + 2
+-- This will yield 7
+
+-- Exercise: Using it for analysis (Enter the following code)
+True
+it && True
+it && False
+it || False
+it || True
+
+-- Notice that this is inadvertently making a truth table like we did
+-- previously.
+
+-- That's all that I have for this brief introductory tutorial. 
+-- Please consider signing up for Mark Jones' CS457/557 class
+-- to learn more about Haskell programming or consider joining an
+-- ACM Open Hack group on Friday afternoons at 4:30PM in the
+-- ACM lounge.
 -- More questions? Ask Erik, and he can offer more tutorials.
