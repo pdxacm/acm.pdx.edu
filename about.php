01506fb0fb54a4e3aace3bc9ea33f7da5b3bf048
Kiarash Torkian <torkian@pdx.edu>
2015-09-02 20:04:01 -0700

    Header fixed

--- about.php
+++ about.php
@@ -11,8 +11,7 @@
     The Association for Computing Machinery is the largest international organization devoted to science and education related to computing.
     It is a non-profit organization founded in 1947 that provides its members access to professional publications, conferences, and career resources.
     The ACM also has student programming competitions.
-  </div>
-    <h3>Mission Statement</h3>
+    <h2>Mission Statement</h2>
     To provide a platform of educational resources to foster a diverse community of people passionate about technology.
   </div>
 </div>
