41012a56683db05f943cff11078b3bb99cb7295b
Kiarash Torkian <torkian@pdx.edu>
2015-09-02 23:21:32 -0700

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,7 @@
   <h1>ACM @ PSU</h1>
   Portland State University Student Chapter of<br />
   the Association for Computing Machinery
-
-
-   
+  <img src= "<?php echo $image ?>" alt="test" width="350" height="181"/>
 </div>
 
 <h2>Welcome New and Returning Students!</h2>
