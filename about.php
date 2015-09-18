49aca7929600bfa59f886093f2a23b5577382953
Kiarash Torkian <torkian@pdx.edu>
2015-09-17 19:11:06 -0700

    Update about.php

--- about.php
+++ about.php
@@ -68,7 +68,7 @@
     It was founded in 1946 and is the only public university in the state that is located in a major metropolitan city.
     <br /><br />
     Our group often hangs out in the Portland State University Fourth Avenue Building (FAB), Room 88<br />
-    1900 SW Fourth Avenue<br /> Portland, Oregon 97201<br />
+    Portland, Oregon 97201<br />
     (<a href="http://maps.google.com/maps?q=1900+SW+Fourth+Avenue%2C+Portland+Oregon+97201+">Google map</a>)
     (<a href="https://www.pdx.edu/floorplans/buildings/fab">FAB</a>)
     (<a href="https://www.pdx.edu/floorplans/buildings/eb">EB</a>)</div>
